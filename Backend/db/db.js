311dc5b4a1185839358693de9f739d19e1f5ca71
Your Name <you@example.com>
2025-10-07 03:46:43 +0530

    Updated backend and frontend code

--- Backend/db/db.js
+++ Backend/db/db.js
@@ -4,6 +4,7 @@ const db = async () => {
     try {
         mongoose.set('strictQuery', false);
         await mongoose.connect(process.env.MONGO_URL);
+
         console.log("Connected to MongoDB");
     } catch (error) {
         console.log('DB connection error:', error);
